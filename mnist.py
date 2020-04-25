5991e596574a57e13228f0472aae56855429b9dc
Leik Lima-Eriksen <leik.lima-eriksen@lyse.net>
2020-04-25 12:11:07 +0200

    mnist: refactor out dataloading from main.

--- mnist.py
+++ mnist.py
@@ -15,20 +15,43 @@ def read_idx(filename):
         shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
         return np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
 
-# Import data:
-def load_train(): 
-    data = []
-    with open('./datasets/mnist_train.pkl', 'rb') as file_train:
-        data = pkl.load(file_train)
-    return np.array(data)
+# Load the first num_samples images from train dataset with  corresponding labels:
+def load_train_dataset(num_samples=10000):
+    # Maximum 60 000 samples
+    MAX_TRAIN_SAMPLES = 60000
+    num_samples = num_samples % MAX_TRAIN_SAMPLES
 
-def load_test():
-    data = []
-    with open('./datasets/mnist_test.pkl', 'rb') as file_test:
-        data = pkl.load(file_test)
-    return np.array(data)
+    # Read from file
+    raw_train = read_idx('./mnist_datasets/train-images.idx3-ubyte')
+    train_labels = read_idx('./mnist_datasets/train-labels.idx1-ubyte')
 
+    # Flatten images
+    train_data = np.reshape(raw_train, (60000, 28*28))
 
+    # Truncate  size to num_samples
+    train_data = train_data[:num_samples]
+    train_labels = train_labels[:num_samples]
+
+    return (train_data, train_labels)
+
+# Load the first num_samples images from test dataset with  corresponding labels:
+def load_test_dataset(num_samples):
+    # Maximum 10000 samples
+    MAX_TEST_SAMPLES = 10000
+    num_samples = num_samples % MAX_TEST_SAMPLES
+
+    # Read from file
+    raw_test = read_idx('./mnist_datasets/t10k-images.idx3-ubyte')
+    test_labels = read_idx('./mnist_datasets/t10k-labels.idx1-ubyte')
+
+    # Flatten images
+    test_data = np.reshape(raw_test, (MAX_TEST_SAMPLES, 28*28))
+
+    # Truncate size to num_samples
+    test_data = test_data[:num_samples]
+    test_labels = test_labels[:num_samples]
+
+    return (test_data, test_labels)
 
 def classify_img(nearest):
     nearest_number_counts = [0]*10
@@ -50,12 +73,12 @@ def k_nearest_neighbours(data, data_labels, image, k_neighbours):
         dist = distance.euclidean(data[i], image)
         dist_and_label_list.append((dist, data_labels[i]))
     dist_and_label_list = sorted(dist_and_label_list, key=lambda dist: dist[0])
-    
+
     labels = [ dist_and_label[1] for dist_and_label in dist_and_label_list[:k_neighbours] ]
     return labels
- 
+
 def get_clusters(dataset, labels, classes, n_clusters=64):
-    label_and_clusters = [] # [ (label, clusters=[]) ] 
+    label_and_clusters = [] # [ (label, clusters=[]) ]
     for curr_class in classes:
         indices = labels == curr_class
         data_of_curr_class = dataset[indices]
@@ -65,7 +88,7 @@ def get_clusters(dataset, labels, classes, n_clusters=64):
         label_and_clusters.append( (curr_class, clusters) )
 
     return label_and_clusters
-        
+
 def get_cluster_dataset(dataset, labels):
     label_and_clusters = get_clusters(dataset, labels, classes=range(10), n_clusters=64)
 
@@ -74,7 +97,7 @@ def get_cluster_dataset(dataset, labels):
     train_labels = np.array(train_labels).reshape(64*10)
     for (label, clusters) in label_and_clusters:
         train_data.extend(clusters)
-        
+
     return (train_data, train_labels)
 
 def plot_misclassified_images(images, labels_predicted, labels_true):
@@ -101,15 +124,19 @@ def plot_correctly_classified_images(images, labels_predicted, labels_true):
         img_plot = ax.imshow(image)
         img_plot.set_interpolation("nearest")
 
-def plot_confusion_matrix(confusion_matrix):
+def plot_confusion_matrix(confusion_matrix, name):
     df_cm = pd.DataFrame(confusion_matrix, index = [i for i in "0123456789"],
                   columns = [i for i in "0123456789"])
-    plt.figure(figsize = (10,7))
+    fig = plt.figure(figsize = (10,7))
+
+    fig.suptitle(name, fontsize=16)
+
     sn.heatmap(df_cm, annot=True)
 
 ######################
 # Runtime functions: #
 ######################
+
 def run_nearest_neighbour_classifier(train_data, train_labels, test_data, test_labels):
     num_errors = 0
     test_predicted = []
@@ -119,43 +146,43 @@ def run_nearest_neighbour_classifier(train_data, train_labels, test_data, test_l
         predicted_number = classify_img(nearest_neighbours)
 
         test_predicted.append(predicted_number)
-        print(test_labels[i])
 
         if predicted_number != test_labels[i]:
             num_errors += 1
 
     error_rate = num_errors/len(test_data)*100
-    print("Error rate:", error_rate, "%")
+    print("Error rate for NN using whole dataset:", error_rate, "%")
 
     confusion_matrix = metrics.confusion_matrix(test_labels, test_predicted)
-    plot_confusion_matrix(confusion_matrix)
+    plot_confusion_matrix(confusion_matrix, name="CM for NN using whole dataset")
 
     #plot_correctly_classified_images(train_data, test_labels, test_predicted)
     #plot_misclassified_images(train_data, test_labels, test_predicted)
-        
-    
-    
+
+
+
 
 def run_k_nearest_neighbour_classifier(train_data, train_labels, test_data, test_labels):
     num_errors = 0
     test_predicted = []
+    NUM_NEIGHBOURS = 4
 
     for i in range(len(test_data)):
-        nearest_neighbours = k_nearest_neighbours(train_data, train_labels, test_data[i], 4)
+        nearest_neighbours = k_nearest_neighbours(train_data, train_labels, test_data[i], NUM_NEIGHBOURS)
         predicted_number = classify_img(nearest_neighbours)
         test_predicted.append(predicted_number)
 
         if predicted_number != test_labels[i]:
             num_errors += 1
-            
+
     error_rate = num_errors/len(test_data)*100
-    print("Error rate =",error_rate,"%")
+    print("Error rate for KNN with k =",NUM_NEIGHBOURS, "using whole dataset:", error_rate,"%")
 
     confusion_matrix = metrics.confusion_matrix(test_labels, test_predicted)
-    plot_confusion_matrix(confusion_matrix)
+    plot_confusion_matrix(confusion_matrix, name="CM FOR KNN using whole dataset")
 
 
-    
+
 def run_cluster_classifier(train_data, train_labels, test_data, test_labels):
     errors = 0
     train_cluster, train_labels = get_cluster_dataset(train_data, train_labels)
@@ -170,33 +197,18 @@ def run_cluster_classifier(train_data, train_labels, test_data, test_labels):
             errors +=1
 
     error_rate = errors/len(test_data)*100
-    print("Error rate =",error_rate,"%")
+    print("Error rate for NN using cluster:", error_rate, "%")
 
     confusion_matrix = metrics.confusion_matrix(test_labels, test_predicted)
-    plot_confusion_matrix(confusion_matrix)
-
-
-
-        
+    plot_confusion_matrix(confusion_matrix, name="CM for NN using cluster")
 
 def main():
 
-    train_size = 60000
-    test_size = 100
+    TRAIN_SIZE = 1000
+    TEST_SIZE = 100
 
-    raw_train = read_idx('./datasets/train-images.idx3-ubyte')
-    train_data = np.reshape(raw_train, (60000, 28*28))
-    train_data= train_data[:train_size]
-    train_labels = read_idx('./datasets/train-labels.idx1-ubyte')
-    train_labels = train_labels[:train_size]
-
-    raw_test = read_idx('./datasets/t10k-images.idx3-ubyte')
-    test_data = np.reshape(raw_test, (10000, 28*28))
-    test_data = test_data[:test_size]
-    test_labels = read_idx('./datasets/t10k-labels.idx1-ubyte')
-    test_labels = test_labels[:test_size]
-
-    
+    train_data, train_labels = load_train_dataset(num_samples=TRAIN_SIZE)
+    test_data, test_labels = load_test_dataset(num_samples=TEST_SIZE)
 
     run_nearest_neighbour_classifier(train_data, train_labels, test_data, test_labels)
     run_k_nearest_neighbour_classifier(train_data, train_labels, test_data, test_labels)
